6e6d8cf9a3cb45f7e0aa4382fcf847a7d0335f9e
Fid <38692385+fidken@users.noreply.github.com>
2025-03-22 10:20:51 +0300

    Delete 3_lab directory

--- 3_lab/task1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$very_bad_unclear_name = "15 chicken wings";
-
-$order = &$very_bad_unclear_name;
-$order .= ' and naggets';
-
-echo "\nYour order is: $very_bad_unclear_name.\n";
--- 3_lab/task7.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function printStringReturnNumber(): int {
-    echo "Строка \n";
-    return 42;
-}
-
-$my_num = printStringReturnNumber();
-echo $my_num;
